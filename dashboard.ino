f5de009e924a610e7a213e2b041f1cc0cb6971fe
Satyam Yadav <satyamyadav13579@gmail.com>
2016-11-30 20:22:36 +0530

    radii decreased

--- dashboard.ino
+++ dashboard.ino
@@ -1,25 +1,20 @@
 #include <UTFT_Geometry.h>
 #include <Keyboard.h>
 
+
 #include <UTFT.h>
 extern uint8_t SevenSegNumFont[];
 extern uint8_t BigFont[];
-extern uint8_t SmallFont[];
 extern uint8_t SevenSegNumFont[];
 const double pi=3.1415;
 UTFT myGLCD(CTE50,38,39,40,41);
 UTFT_Geometry geo(&myGLCD);
 void fillarc(int cirnum,int value){
   int c=180+cirnum*440;
-  geo.drawArc(c,265,105,120,120+3*value,30);
- 
+  geo.drawArc(c,265,105,120,120+3*value,10);
   }
-
-
-
-
 void setup() {
-  
+  Serial.begin(9600);
   myGLCD.InitLCD();
   myGLCD.setFont(BigFont);
   myGLCD.clrScr();
@@ -31,10 +26,10 @@ void setup() {
   myGLCD.setColor(53,126,199);
   //myGLCD.fillRect(560,60,640,79);
   myGLCD.setColor(29,39,99);
-  myGLCD.drawCircle(180, 265,140);
-  myGLCD.drawCircle(620, 265,140);
+  myGLCD.drawCircle(180, 265,130);
+  myGLCD.drawCircle(620, 265,130);
   
-  myGLCD.drawCircle(180, 265,89);
+  myGLCD.drawCircle(180, 265,99);
   myGLCD.drawCircle(620, 265,89);
 
   
@@ -44,16 +39,16 @@ void setup() {
      myGLCD.setColor(0,0,0);
   for(int i=1;i<10;i++){
     int y=30+30*i;
-  myGLCD.drawLine(x1+121*cos(y*pi/180),y1+121*sin(y*pi/180),x1+139*cos(pi*y/180),y1+139*sin(pi*y/180));
+  myGLCD.drawLine(x1+111*cos(y*pi/180),y1+111*sin(y*pi/180),x1+129*cos(pi*y/180),y1+129*sin(pi*y/180));
     if(y<=120){
-      myGLCD.printNumI(10*i,x1+(165)*cos(pi*(y+4)/180),y1+(165)*sin(pi*(y+4)/180));  
+      myGLCD.printNumI(10*i,x1+(155)*cos(pi*(y+4)/180),y1+(155)*sin(pi*(y+4)/180));  
     }
     if(y>120&&y<240){
-      myGLCD.printNumI(10*i,x1+(172)*cos(pi*(y+4)/180),y1+(172)*sin(pi*(y+4)/180));
+      myGLCD.printNumI(10*i,x1+(162)*cos(pi*(y+4)/180),y1+(162)*sin(pi*(y+4)/180));
       }
     if(y>=240){
       
-      myGLCD.printNumI(10*i,x1+(165)*cos(pi*(y-4)/180),y1+(165)*sin(pi*(y-4)/180));  
+      myGLCD.printNumI(10*i,x1+(155)*cos(pi*(y-4)/180),y1+(155)*sin(pi*(y-4)/180));  
     }
     
     }
@@ -61,9 +56,9 @@ void setup() {
     for(int j=0;j<=1;j++){
       int ang1=28+j;
       int ang2=331+j;
-     myGLCD.drawLine(x1+90*cos(ang1*pi/180),y1+90*sin(ang1*pi/180),x1+139*cos(pi*ang1/180),y1+139*sin(pi*ang1/180));
+     myGLCD.drawLine(x1+100*cos(ang1*pi/180),y1+100*sin(ang1*pi/180),x1+129*cos(pi*ang1/180),y1+129*sin(pi*ang1/180));
      
-    myGLCD.drawLine(x1+90*cos(ang2*pi/180),y1+90*sin(ang2*pi/180),x1+139*cos(pi*ang2/180),y1+139*sin(pi*ang2/180));
+    myGLCD.drawLine(x1+100*cos(ang2*pi/180),y1+100*sin(ang2*pi/180),x1+129*cos(pi*ang2/180),y1+129*sin(pi*ang2/180));
     }
   }
   myGLCD.setColor(53,126,199);
@@ -78,14 +73,15 @@ myGLCD.setFont(SevenSegNumFont);
 }
 
 void loop() {
-  int battery=90;//battery percentage value
-  int spee=45;
+  while(Serial.available()>0){
+  int spee=Serial.parseInt();
+  int battery=Serial.parseInt();
    myGLCD.printNumI(spee,140,245);
    //printing the speed value
    //myGLCD.fillRect(505,(435-3.5*x),695,435);
     myGLCD.printNumI(battery,580,245);
    
    fillarc(0,spee);
-   fillarc(1,battery);
+   fillarc(1,battery);}
    delay(2000);
 }
